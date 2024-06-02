2cea3af39fb7d6a36f58eb8c3a5b276d78c01eca
David Akim <davidjohnakim@hotmail.com>
2024-06-01 20:43:55 -0400

    Fixed vite.config.ts

--- dist/assets/NoPage-01ebac49.js
+++
@@ -1,2 +0,0 @@
-import{bo as o}from"./vendor-cc8277fd.js";import"./fluentui-react-e5b42e96.js";function r(){return o.jsx("h1",{children:"404"})}r.displayName="NoPage";export{r as Component};
-//# sourceMappingURL=NoPage-01ebac49.js.map
---
+++ dist/assets/NoPage-faa228ee.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-dc12ddf1.js";function e(){return o.jsx("h1",{children:"404"})}e.displayName="NoPage";export{e as Component};
